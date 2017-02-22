b4a754eeab7b7ce96b3ff9489e47138945c29b43
Dave Lugg <dave.lugg@gmail.com>
2017-02-21 22:36:20 -0500

    Refactored some radio menu code. Adds ForEachRange function to operate on all ranges

--- TNN4.lua
+++ TNN4.lua
@@ -1,42 +1,42 @@
-function SpawnManager()
-  local spawns = {
+function RangeManager()
+  local ranges = {
       ["easy_range"] = { ['spawner'] = SPAWN:New("Easy Range"), 
                          ['zone'] = ZONE:New("Easy Range"),
                          ['type'] = 'ground',
-                         ['smoke_color'] = SMOKECOLOR.Blue
+                         ['smoke_color'] = SMOKECOLOR.Blue,
+                         ['label'] = "Easy"
                        }
   }
   
-  function SpawnGroup(self, group)
-    self.Spawns[group]['group'] = self.Spawns[group]['spawner']:ReSpawn()
-    return self.Spawns[group]['group']
+  function SpawnRange(self, range)
+    self.Ranges[range]['group'] = self.Ranges[range]['spawner']:ReSpawn()
+    return self.Ranges[range]['group']
   end
   
-  return { SpawnGroup = SpawnGroup,
-           Spawns = spawns }
-end
-
-spawnManager = SpawnManager()
-
-function RangeSmoke(difficulty)
-  ZONE:New(difficulty .. " Range"):SmokeZone(SMOKECOLOR.Blue, 4)
-end
-
-function AddRangeSmokeRadioMenus()
-  local smoke_parent_menu = MENU_MISSION:New("Smoke")
-  local difficulties = {"Easy"} 
-  for idx, difficulty in ipairs(difficulties) do
-    MENU_MISSION_COMMAND:New(difficulty,smoke_parent_menu,RangeSmoke,difficulty)
+  function ForEachRange(self, fn)
+    for idx, range in pairs(self.Ranges) do
+      fn(idx, range)
+    end
   end
+  
+  return { SpawnRange = SpawnRange,
+           Ranges = ranges,
+           ForEachRange = ForEachRange }
 end
 
-function AddRangeRespawnRadioMenus()
-  local smoke_parent_menu = MENU_MISSION:New("Smoke")
-  local difficulties = {"Easy"} 
-  for idx, difficulty in ipairs(difficulties) do
-    MENU_MISSION_COMMAND:New(difficulty,smoke_parent_menu,RangeSmoke,difficulty)
-  end
+rangeManager = RangeManager()
+
+function RangeSmoke(range)
+  range.zone:SmokeZone(range.smoke_color, 4)
 end
 
-AddRangeSmokeRadioMenus()
-AddRangeRespawnRadioMenus()
+function CreateRangeRadioMenus()
+  local ranges_parent_menu = MENU_MISSION:New("Ranges")
+  rangeManager:ForEachRange(function(range_name, range)
+    local range_parent_menu = MENU_MISSION:New(range.label, ranges_parent_menu)
+    MENU_MISSION_COMMAND:New("Start Smoke", range_parent_menu, RangeSmoke, range)
+    MENU_MISSION_COMMAND:New("Respawn", range_parent_menu, rangeManager.SpawnRange, rangeManager, range_name)
+  end)
+end
+
+CreateRangeRadioMenus()
